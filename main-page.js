62fce6c6ce97f921d2c1ca62e9d298696de46e5f
Dimitar Kerezov <dimitar.kerezov@telerik.com>
2016-03-01 16:00:22 +0200

    Simplify the code

--- main-page.js
+++ main-page.js
@@ -1,6 +1,7 @@
-var vmModule = require("./main-view-model");
+var viewModel = require("./main-view-model");
 function pageLoaded(args) {
     var page = args.object;
-    page.bindingContext = vmModule.mainViewModel;
+    page.bindingContext = viewModel;
 }
+
 exports.pageLoaded = pageLoaded;
